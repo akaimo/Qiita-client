41db18d0f569eb95d3d2a4c7338b05c45718567d
shuhei <kawashu0723@gmail.com>
2016-02-09 16:29:46 +0900

    Initial Commit

---
+++ Qiita-clientTests/Qiita_clientTests.swift
@@ -0,0 +1,36 @@
+//
+//  Qiita_clientTests.swift
+//  Qiita-clientTests
+//
+//  Created by Shuhei Kawaguchi on 2016/02/09.
+//  Copyright © 2016年 Shuhei Kawaguchi. All rights reserved.
+//
+
+import XCTest
+@testable import Qiita_client
+
+class Qiita_clientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

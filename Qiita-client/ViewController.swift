bf4666732094d67431d4a81e127aa5fb56fd1072
shuhei <kawashu0723@gmail.com>
2016-02-10 14:11:31 +0900

    add datasource

--- Qiita-client/ViewController.swift
+++ Qiita-client/ViewController.swift
@@ -9,11 +9,19 @@
 import UIKit
 import APIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var homeTableView: UITableView!
+    
+    let dataSource = ["login", "user", "stock", "new"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Qiita"
+        self.homeTableView.dataSource = self
+        self.homeTableView.delegate = self
+        self.homeTableView.tableFooterView = UIView()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -35,7 +43,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dataSource.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = self.dataSource[indexPath.row]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print(indexPath.row)
+    }
 
 }
 

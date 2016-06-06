600e8ae4d85afcac052c00e5bb8dd3a15b97e347
shuhei <kawashu0723@gmail.com>
2016-06-06 15:47:07 +0900

    a

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bundle install --path=vendor/bundle
+
+if ! diff Cartfile.resolved Carthage/Cartfile.resolved &>/dev/null; then
+  carthage bootstrap --platform ios
+  cp Cartfile.resolved Carthage
+fi
